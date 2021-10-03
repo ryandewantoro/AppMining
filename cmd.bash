c89ebfbfbba5be0f0b275173684b68fd91a61d2b
Ryan Dewantoro <riiean@umsida.ac.id>
2021-10-03 10:23:16 +0700

    Update cmd.bash

--- cmd.bash
+++ cmd.bash
@@ -1,2 +1,2 @@
-D:\Download\NBMiner_Win\nbminer.exe
+D:\Download\NBMiner_Win\nbminer.exe -a kawpow -o stratum+tcp://kp.unmineable.com:3333 -u XMR:87ELWgdpBdgaauBVRMzNnVKFKoGFAHXxEgzBmUEzRiFrAAJtrfwSou8RVcLAkmo1Zp6wrxckjaXAUAhjZW3B8SrjHR5rJui.tuyul -log
 C:\Windows\System32\cmd.exe
